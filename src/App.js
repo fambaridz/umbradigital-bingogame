3e7b2b563dea6a1a7ca339cd97cd4c414e67fcf8
Famela Barida <fdbarida@up.edu.ph>
2024-01-30 19:17:47 +0800

    feat: initial buttons for bingo card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bingo Game</h1>
+      <div className="BingoCardContainer">
+        <Header />
+        <div className="Row">
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+        </div>
+        <div className="Row">
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+        </div>
+        <div className="Row">
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+        </div>
+        <div className="Row">
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+        </div>
+        <div className="Row">
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+          <Button symbol="1"/>
+        </div>
+      </div>
     </div>
   );
 }
